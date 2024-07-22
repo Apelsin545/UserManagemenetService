ffb0b7f5c01e114399cad5bc493b490326bff938
Dmitrii <dmitri_ch@list.ru>
2024-07-22 15:04:28 +0300

    feat: создать сущность User

---
+++ src/main/java/ru/eventplanner/usermanagementservice/user/User.java
@@ -0,0 +1,23 @@
+package ru.eventplanner.usermanagementservice.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor(force = true)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private final Long id;
+
+    private String name;
+
+    @JsonProperty("registration_date")
+    private LocalDate creationDate;
+}
